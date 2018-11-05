d2ddbe4f99ea7520d8104268ef35eb37a54af2c5
Andrii MAKARENKO <amakaren@e2r12p11.unit.ua>
2018-11-05 22:40:55 +0200

    Created some stuff that is probably not working

--- Additional/ft_memalloc.c
+++ Additional/ft_memalloc.c
@@ -6,8 +6,20 @@
 /*   By: amakaren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 21:20:58 by amakaren          #+#    #+#             */
-/*   Updated: 2018/11/03 21:21:00 by amakaren         ###   ########.fr       */
+/*   Updated: 2018/11/05 20:47:13 by amakaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*ft_memalloc(size_t size)
+{
+	void	*result;
+	char	*nulls;
+	int		i;
 
+	result = malloc(size);
+	nulls = (char*)result;
+	i = -1;
+	while (++i < size)
+		nulls[i] = '0';
+	return (result);
+}
--- Includes/ft_functions.h
+++ Includes/ft_functions.h
@@ -1,11 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
+/*   ft_functions.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amakaren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/05 20:39:08 by amakaren          #+#    #+#             */
-/*   Updated: 2018/11/05 20:39:09 by amakaren         ###   ########.fr       */
+/*   Created: 2018/11/05 20:50:08 by amakaren          #+#    #+#             */
+/*   Updated: 2018/11/05 20:50:09 by amakaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_FUNCTIONS_H
+# define FT_FUNCTIONS_H
+
+void	ft_memset(void *b, int n, size_t len);
+void	*ft_memalloc(size_t size);
+#endif
