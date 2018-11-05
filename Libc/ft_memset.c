5ff8dea5306fb64a92f9d3b289f86b1f809ff758
Andrii MAKARENKO <amakaren@e2r12p11.unit.ua>
2018-11-05 22:58:33 +0200

    Created memtest, it's likely to work.

--- Libc/ft_memset.c
+++ Libc/ft_memset.c
@@ -10,4 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_memset(void *b, int c, size_t len)
+{
+	int		i;
+	char	*nulls;
 
+	nulls = (char*)b;
+	i = -1;
+	while (++i < len)
+		nulls[i] = (unsigned char)c;
+	return (b);
+}
