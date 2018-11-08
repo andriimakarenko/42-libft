615581685d2d5a95b08b46d8656cb76937f15a90
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-08 20:40:04 +0200

    Created strequ

--- Additional/ft_strequ.c
+++ Additional/ft_strequ.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strequ(char const *s1, char const *s2)
+{
+	int i;
+	int result;
 
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (s1[i] == s2[i] == '\0')
+		return (1);
+	return (0);
+}
