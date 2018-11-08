f6c3b1cda9b85360228416de3aa22756381cc1c3
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-08 23:06:16 +0200

    Created strim

--- Additional/ft_strtrim.c
+++ Additional/ft_strtrim.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#define SLN ft_strlen
+#define SWS ft_count_start_ws
+#define EWS ft_count_end_ws
 
+#include "../Includes/libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s)
+{
+	char	*result;
+	int		start_ws;
+	int		end_ws;
+	int		i;
+
+	if (s == NULL)
+		return (NULL);
+	start_ws = ft_count_start_ws(s);
+	end_ws = ft_count_end_ws(s);
+	result = (char*)malloc(sizeof(char) * (SLN(s) - SWS(s) - EWS(s) + 1));
+	if (result == NULL)
+		return (NULL);
+	i = start_ws - 1;
+	while (++i + end_ws < ft_strlen(s))
+		result[i] = s[i + start_ws];
+	return (result);
+}
