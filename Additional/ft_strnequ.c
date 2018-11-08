23e149ca4a21869a56041d36e695c378d4964c39
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-08 23:16:22 +0200

    Included <stddef.h> wherever size_t is used, but it doesn't seem 2 help

--- Additional/ft_strnequ.c
+++ Additional/ft_strnequ.c
@@ -11,13 +11,14 @@
 /* ************************************************************************** */
 
 #include "../Includes/libft.h"
+#include <stddef.h>
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	int i;
 
 	if (s1 == NULL || s2 == NULL)
-		return (0)
+		return (0);
 	i = 0;
 	while (s1[i] && s2[i] && i < n)
 	{
--- Additional/ft_strsub.c
+++ Additional/ft_strsub.c
@@ -12,13 +12,14 @@
 
 #include "../Includes/libft.h"
 #include <stdlib.h>
+#include <stddef.h>
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
 	int		i;
 
-	result = (char*)malloc(sizeof(char) * (len + 1))
+	result = (char*)malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
 	i = -1;
