4469e41499689622f93100ae6a35b188c4c75a91
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-08 22:20:26 +0200

    Created strjoin, updated headers

--- Additional/ft_strjoin.c
+++ Additional/ft_strjoin.c
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	int		i;
+	int		r;
+
+	result = (char*)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (result == NULL)
+		return (NULL);
+	i = -1;
+	r = -1;
+	while (s1[++i])
+		result[++r] = s1[i];
+	i = -1;
+	while (s2[++i])
+		result[++r] = s2[i];
+	result[r + 1] = '\0';
+	return (result);
+}
