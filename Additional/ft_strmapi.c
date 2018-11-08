3753c2b22c5ac99cdea7c5dcd09a60fd62192388
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-08 16:39:38 +0200

    Created strmapi, updated the headers

--- Additional/ft_strmapi.c
+++ Additional/ft_strmapi.c
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	int		len;
+	char	*result;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	i = -1;
+	len = 0;
+	while (s[++i])
+		len++;
+	result = (char*)malloc(sizeof(char) * (len + 1));
+	result[len + 1] = '\n';
+	i = -1;
+	while (result[++i])
+		f(i, result[i]);
+	return (result);
+}
