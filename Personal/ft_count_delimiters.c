af2b90e54144f7ee6a63590db0ab3ad6f8167652
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-11-09 17:02:33 +0200

    Created a function to count delimiters

---
+++ Personal/ft_count_delimiters.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_delimiters.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakaren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 16:58:12 by amakaren          #+#    #+#             */
+/*   Updated: 2018/11/09 17:01:27 by amakaren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/libft.h"
+
+int	ft_count_delimiters(char *s, char c)
+{
+	int i;
+	int result;
+
+	result = 0;
+	i = -1;
+	while (str[++i])
+		if (str[i] == c && str[i + 1] && str[i + 1] != c)
+			result++;
+	return (result);
+}
