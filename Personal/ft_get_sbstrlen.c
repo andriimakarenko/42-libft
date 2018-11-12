be64c5c5984ff0ec7cbcb904c94ef5d14af7cf5c
Andrii MAKARENKO <amakaren@e2r10p18.unit.ua>
2018-11-12 22:09:35 +0200

    Included the headers in that counter too

--- Personal/ft_get_sbstrlen.c
+++ Personal/ft_get_sbstrlen.c
@@ -6,9 +6,11 @@
 /*   By: amakaren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 21:59:59 by amakaren          #+#    #+#             */
-/*   Updated: 2018/11/12 22:04:25 by amakaren         ###   ########.fr       */
+/*   Updated: 2018/11/12 22:09:15 by amakaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../Includes/libft.h"
 
 int	ft_get_sbstrlen(const char *s, int start, char delimiter)
 {
