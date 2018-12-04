b1238e79d06ce2f53c8924fdae7f3efd7d9f4fb7
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-04 20:53:41 +0200

    Created memmove

--- Libc/ft_memmove.c
+++ Libc/ft_memmove.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	int			i;
+	char		*d;
+	const char	*s;
+
+	d = (char*)dst;
+	s = (const char*)src;
+	i = -1;
+	while (++i < n)
+		d[i] = s[i];
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char buffer[len];
+
+	ft_memcpy(buffer, src, n);
+	ft_memcpy(dst, buffer, n);
+	return (dst);
+}
