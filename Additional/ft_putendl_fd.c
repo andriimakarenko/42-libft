ca309c1d0bf9cb97fbeca57725fc1964a1d52223
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-04 20:09:08 +0200

    Created putendl_fd

--- Additional/ft_putendl_fd.c
+++ Additional/ft_putendl_fd.c
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
+#include <unistd.h>
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char const *s, int fd)
+{
+	int i;
+
+	i = -1;
+	while (s[++i])
+		ft_putchar(s[i], fd);
+}
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
