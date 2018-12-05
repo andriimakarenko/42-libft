ed9f89f4efcb798ad0c6ab8e995b3d0efd2ffa3e
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-05 12:45:51 +0200

    Created strncpy

--- Includes/libft.h
+++ Includes/libft.h
@@ -52,4 +52,5 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char * dst, const char * src, size_t len);
 #endif
--- Libc/ft_strncpy.c
+++ Libc/ft_strncpy.c
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	int i;
+
+	if (!(src) || !(dst))
+		return (NULL);
+	i = 0;
+	while (src[i] && i < len)
+		dst[i] = src[i++];
+	if (!(src[i]) &&  i < len)
+		while(i < len + 1)
+			dst[i++] = '\0';
+}
