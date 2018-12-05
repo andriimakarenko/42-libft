5e4a87fac84debb98858ff5a9dffe4d4d4fe12a6
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-05 12:59:55 +0200

    Created strcat

--- Libc/ft_strcat.c
+++ Libc/ft_strcat.c
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+char	*ft_strcat(char *s1, const char *s2)
+{
+	int i;
+	int j;
+
+	if (!(s1))
+		return (NULL);
+	if (!(s2))
+		return (s1);
+	i = ft_strlen(s1);
+	j = -1;
+	while (s2[++j])
+		s1[i+j] = s2[j];
+	s1[i+j] = '\0';
+	return (s1);
+}
