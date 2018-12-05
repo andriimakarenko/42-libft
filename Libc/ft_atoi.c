aefb3e7b0c3aa7f91a58cf5363ce8b5eb6065358
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-05 18:49:48 +0200

    Created and tested ft_atoi

--- Libc/ft_atoi.c
+++ Libc/ft_atoi.c
@@ -10,4 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+int		ft_shift_dec_rank(int base, int rank)
+{
+	int i;
+
+	i = 0;
+	while (++i < rank)
+		base *= 10;
+	return (base);
+}
+
+int		ft_atoi(const char *str)
+{
+	int		result;
+	int		i;
+	int		digit_len;
+	int     sign;
+
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+		i++;
+	if (str[i] == '+')
+		i++;
+	if (str[i] == '-' && (sign = -1) == -1)
+		i++;
+	if (str[i] < '0' || str[i] > '9')
+		return (0);
+	digit_len = 1;
+	while (str[i + digit_len] >= '0' && str[i + digit_len] <= '9')
+		digit_len++;
+	while (digit_len > 0)
+	{
+		result += ft_shift_dec_rank((str[i++] - 48), digit_len);
+		digit_len--;
+	}
+	return (result * sign);
+}
