0605d8661f303aa6f6f53c9c45d3b3dfedca6c6d
Andrii MAKARENKO <amakaren@e2r4p11.unit.ua>
2018-12-05 15:47:13 +0200

    Created strstr

--- Libc/ft_strstr.c
+++ Libc/ft_strstr.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/libft.h"
 
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	char	trigger;
+	int		i;
+	int		j;
+
+	trigger = *needle;
+	if (trigger = '\0')
+		return (haystack);
+	i = -1;
+	while (haystack[++i])
+	{
+		if (haystack[i] == trigger && (j = 0) == 0)
+		{
+			while (haystack[i + j] == needle[j] && needle[j])
+				j++;
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return (NULL);
+}
