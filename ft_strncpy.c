b37dc4e40fde984bc4adc174d31d15bc23a9fa53
Andy Mac <andymac@localhost.localdomain>
2018-12-06 02:14:23 +0200

    Strncpy now compiles

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -14,14 +14,18 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	int i;
+	size_t i;
 
 	if (!(src) || !(dst))
 		return (NULL);
 	i = 0;
 	while (src[i] && i < len)
-		dst[i] = src[i++];
+	{
+		dst[i] = src[i];
+		i++;
+	}
 	if (!(src[i]) &&  i < len)
 		while(i < len + 1)
 			dst[i++] = '\0';
+	return (dst);
 }
