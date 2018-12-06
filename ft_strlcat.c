84383b5059fb41ac9d90c1b3927397b6dd049ab9
Andy Mac <andymac@localhost.localdomain>
2018-12-06 14:07:03 +0200

    Strlcat works, but it's a complete and utter WTF

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,14 +17,25 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t dst_init_len;
 	size_t total_len;
+	size_t n;
 	int i;
 	int limit;
 
-	dst_init_len = ft_strlen(dst);
+	dst_init_len = 0;
+	n = dstsize;
+	i = -1;
+	while (n > 0 && dst[++i])
+	{
+	    dst_init_len++;
+	    n--;
+	}
+	if (n == 0)
+	    return (dst_init_len + ft_strlen(src));
 	total_len = dst_init_len + ft_strlen(src);
 	limit = dstsize - dst_init_len - 1;
 	i = -1;
 	while (src[++i] && i < limit)
 		dst[dst_init_len + i] = src[i];
+	dst[dst_init_len + i] = '\0';
 	return (total_len);
 }
