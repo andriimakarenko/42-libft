6f931c4bf523255e7db6e7edc5227deac8bd36cd
Andy Mac <andymac@localhost.localdomain>
2018-12-06 02:19:47 +0200

    Removed memcpy as a subfunction of memmove, as it is there standalone

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,25 +12,11 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	int			i;
-	char		*d;
-	const char	*s;
-
-	d = (char*)dst;
-	s = (const char*)src;
-	i = -1;
-	while (++i < n)
-		d[i] = s[i];
-	return (dst);
-}
-
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char buffer[len];
 
-	ft_memcpy(buffer, src, n);
-	ft_memcpy(dst, buffer, n);
+	ft_memcpy(buffer, src, len);
+	ft_memcpy(dst, buffer, len);
 	return (dst);
 }
