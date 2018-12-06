12893856a415f3c4a1b3d220fc111c1e5c0b7525
Andy Mac <andymac@localhost.localdomain>
2018-12-06 20:12:56 +0200

    Typedefined s_list in headers file

--- libft.h
+++ libft.h
@@ -14,6 +14,13 @@
 # define LIBFT_H
 # include <string.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
 void	*ft_memset(void *b, int n, size_t len);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
