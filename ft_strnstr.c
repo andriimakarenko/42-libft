4a6117e66fca9f6088ad5abe13492b86b378af30
Andrii MAKARENKO <amakaren@e2r4p1.unit.ua>
2018-12-07 16:49:43 +0200

    Fixed alignment in header; renamed haystack & needle to s1 & s2 (revrt!)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,22 +12,22 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t		i;
 	size_t		j;
 
-	if (*needle == '\0')
-		return ((char *)haystack);
+	if (*s2 == '\0')
+		return ((char *)s1);
 	i = -1;
-	while (haystack[++i] && i < len)
+	while (s1[++i] && i < len)
 	{
-		if (haystack[i] == *needle && (j = 0) == 0)
+		if (s1[i] == *s2 && (j = 0) == 0)
 		{
-			while (haystack[i + j] == needle[j] && needle[j])
+			while (s1[i + j] == s2[j] && s2[j])
 				j++;
-			if (needle[j] == '\0' && i + j < len)
-				return ((char *)haystack + i);
+			if (s2[j] == '\0' && i + j < len)
+				return ((char *)s1 + i);
 		}
 	}
 	return (NULL);
